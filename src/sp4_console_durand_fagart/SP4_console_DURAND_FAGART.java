c984458d7d966d28e25216ecc69cd1458fc1b8b2
colombe.fagart@epfedu.fr <33768@DESKTOP-O7O9TDJ.education.lakanal.epf.fr>
2022-11-18 10:47:11 +0100

    creation d'un nouveau projet super puissance 4

---
+++ src/sp4_console_durand_fagart/SP4_console_DURAND_FAGART.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sp4_console_durand_fagart;
+
+/**
+ *
+ * @author 33768
+ */
+public class SP4_console_DURAND_FAGART {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
