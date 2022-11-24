8787dd7abcf43f33419434e0db15dbd3119d66a3
colombe.fagart@epfedu.fr <33768@DESKTOP-O7O9TDJ.education.lakanal.epf.fr>
2022-11-24 14:08:02 +0100

    code presque inchangé: je fais un deuxieme test car j'ai changé de pc et je veux pour voir si tout marche bien avec github et les commit

---
+++ src/sp4_console_durand_fagart/CelluleDeGrille.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_durand_fagart;
+
+/**
+ *
+ * @author colombe
+ */
+public class CelluleDeGrille {
+    
+    private Jeton jetonCourant;
+    public CelluleDeGrille(Jeton jeton_courant) {
+        jetonCourant=jeton_courant;  
+}
+    public void CelluleDeGrille (){
+    jetonCourant=null;
+    }
+    public boolean presenceJeton(){
+        if(jetonCourant==null){
+        return(false);}
+        else{
+        return(true);}
+    }
+    public String lireCouleurDuJeton(){
+    if (presenceJeton()==true){
+        if("jaune".equals(jetonCourant.lireCouleur())){
+        return("jaune");}
+        else{return("rouge");}
+        
+    }
+    else{return("vide");}
+    }
+    public Jeton recupererJeton(){
+    Jeton c;
+    c=jetonCourant;
+    jetonCourant=null;
+    return(c);
+    }
+
+
+}
