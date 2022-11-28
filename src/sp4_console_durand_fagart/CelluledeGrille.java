2dee83f87807dda4013a5dec9fd3b4c3077b02a9
Corentin DURAND <Corentin DURAND@LAPTOP-NGGEAO1O.education.lakanal.epf.fr>
2022-11-28 14:12:20 +0100

    TP Puissance 4
    test

--- src/sp4_console_durand_fagart/CelluledeGrille.java
+++ src/sp4_console_durand_fagart/CelluledeGrille.java
@@ -8,13 +8,13 @@ package sp4_console_durand_fagart;
  *
  * @author 33768
  */
-public class CelluleDeGrille {
+public class CelluledeGrille {
 
     private Jeton jetonCourant;
     private boolean avoirTrouNoir;
     private boolean avoirDesintegrateur;
 
-    public CelluleDeGrille(Jeton jeton_courant) {
+    public CelluledeGrille(Jeton jeton_courant) {
         jetonCourant = jeton_courant;
     }
 
@@ -42,55 +42,72 @@ public class CelluleDeGrille {
             return ("vide");
         }
     }
-    
-    public void affecterJeton(Jeton j){
+
+    public void affecterJeton(Jeton j) {
         jetonCourant = j;
     }
-    
-    public void placeTrouNoir(){
+
+    public void placeTrouNoir() {
         avoirTrouNoir = true;
     }
-    
-    public void supprimerTrouNoir(){
+
+    public void supprimerTrouNoir() {
         avoirTrouNoir = false;
     }
-    
-    public boolean presenceTrouNoir(){
-        if(avoirTrouNoir){
-            return(true);
-        }else{
-            return(false);
+
+    public boolean presenceTrouNoir() {
+        if (avoirTrouNoir) {
+            return (true);
+        } else {
+            return (false);
         }
     }
-    
-    public Jeton recupererJeton(){
-        Jeton temp_jeton; 
+
+    public Jeton recupererJeton() {
+        Jeton temp_jeton;
         temp_jeton = jetonCourant;
-        jetonCourant = null; 
-        return(temp_jeton);
-        
+        jetonCourant = null;
+        return (temp_jeton);
+
     }
-    
-    public void supprimerJeton(){
-        jetonCourant = null; 
+
+    public void supprimerJeton() {
+        jetonCourant = null;
     }
-    
-    public boolean presenceDesintegrateur(){
-        if(avoirDesintegrateur){
-          return(true);
-        } else{
-            return(false);
+
+    public boolean presenceDesintegrateur() {
+        if (avoirDesintegrateur) {
+            return (true);
+        } else {
+            return (false);
         }
     }
-    
-    public void placerDesintegrateur(){
+
+    public void placerDesintegrateur() {
         avoirDesintegrateur = true;
     }
-    
-    public void supprimerDesintegrateur(){
-        avoirDesintegrateur  = false;
+
+    public void supprimerDesintegrateur() {
+        avoirDesintegrateur = false;
+    }
+
+    public void activerTrouNoir() {
+        this.supprimerJeton();
+        this.supprimerTrouNoir();
     }
     
+    @Override
+    public String toString(){
+        if(jetonCourant != null){
+            return("J");
+        }
+        if(avoirTrouNoir){
+            return("@");
+        }
+        if(avoirDesintegrateur){
+            return("D");
+        }
+        return("."); 
     
-
+    }
 }
