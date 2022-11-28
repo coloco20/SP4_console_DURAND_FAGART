7b172196bad40e43fac5d6174c963053507b102f
Corentin DURAND <Corentin DURAND@LAPTOP-NGGEAO1O.education.lakanal.epf.fr>
2022-11-28 14:13:48 +0100

    TP Puissance 4
    PlatauDeJeu

--- src/sp4_console_durand_fagart/PlateauDeJeu.java
+++ src/sp4_console_durand_fagart/PlateauDeJeu.java
@@ -9,18 +9,18 @@ package sp4_console_durand_fagart;
  * @author 33768
  */
 public class PlateauDeJeu {
-    CelluleDeGrille[][] grille = new CelluleDeGrille [6][7];
+    CelluledeGrille[][] grille = new CelluledeGrille [6][7];
     public PlateauDeJeu() {
         for(int i = 0;i<7; i++) {
       for(int j = 0;j<8; j++) {
-          grille[i][j]=new CelluleDeGrille(null);
+          grille[i][j]=new CelluledeGrille(null);
       }}
     
 }
   public int ajouterJetonDansColonne(Jeton jeton_param,int indice){
       for(int i = 0;i<7; i++) {
-          if (grille[i][indice]== new CelluleDeGrille(null)){
-       grille[i][indice]=new CelluleDeGrille(jeton_param);
+          if (grille[i][indice]== new CelluledeGrille(null)){
+       grille[i][indice]=new CelluledeGrille(jeton_param);
           return(i) ;    
           }
           }
@@ -31,7 +31,7 @@ public class PlateauDeJeu {
   public boolean grilleRemplie(){
       for(int i = 0;i<7; i++) {
       for(int j = 0;j<8; j++) {
-          if(grille[i][j]== new CelluleDeGrille(null)){
+          if(grille[i][j]== new CelluledeGrille(null)){
           }
       }}
   return(true);}
