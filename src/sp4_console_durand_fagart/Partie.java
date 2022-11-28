072035723fea0bfddaaff6448df3f065a6e82246
Corentin DURAND <Corentin DURAND@LAPTOP-NGGEAO1O.education.lakanal.epf.fr>
2022-11-28 15:02:36 +0100

    TP Puissance 4
    Class Joueur Complette + modification 1

--- src/sp4_console_durand_fagart/Partie.java
+++ src/sp4_console_durand_fagart/Partie.java
@@ -9,7 +9,31 @@ package sp4_console_durand_fagart;
  * @author 33768
  */
 public class Partie {
-    public Partie() {
+
+    private Joueur tab[] = new Joueur[2];
+    private Joueur joueurCourant;
+    private PlateauDeJeu plateau;
+
+    public Partie(Joueur r, Joueur j) {
+        tab[0] = r;
+        tab[1] = j;
+        // incomplette
+
+    }
     
-}
+    public void attribuerCouleurAuxJoueurs(){
+   
+        tab[0].affecterCouleur("r");
+        tab[1].affecterCouleur("j");
+        
+    }
+    
+    public void creeEtAffecterJeton(Joueur jr){
+        for(int i = 0; i < 31; i++){
+            Jeton j = new Jeton(jr.lireCouleur());
+            jr.ajouterJeton(j);
+        }
+    }
+    
+        
 }
