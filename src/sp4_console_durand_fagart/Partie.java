9cfe59f36081293adde43938221c2591aafc9a2c
Corentin DURAND <Corentin DURAND@LAPTOP-NGGEAO1O.education.lakanal.epf.fr>
2022-11-28 15:30:11 +0100

    TP Puissance 4
    Classe Partie Complette

--- src/sp4_console_durand_fagart/Partie.java
+++ src/sp4_console_durand_fagart/Partie.java
@@ -17,38 +17,53 @@ public class Partie {
     public Partie(Joueur r, Joueur j) {
         tab[0] = r;
         tab[1] = j;
-        // incomplette
+        plateau = new PlateauDeJeu();
 
     }
-    
-    public void attribuerCouleurAuxJoueurs(){
-   
+
+    public void attribuerCouleurAuxJoueurs() {
+
         tab[0].affecterCouleur("r");
         tab[1].affecterCouleur("j");
-        
+
     }
-    
-    public void creeEtAffecterJeton(Joueur jr){
-        for(int i = 0; i < 31; i++){
+
+    public void creeEtAffecterJeton(Joueur jr) {
+        for (int i = 0; i < 31; i++) {
             Jeton j = new Jeton(jr.lireCouleur());
             jr.ajouterJeton(j);
         }
     }
-    
-    public void placerTrouNoirEtDesintegrateur(){
-        
+
+    /*
+    public void placerTrouNoirEtDesintegrateur() {
+        int n;
+        int m;
+        for (int i = 0; i < 6; i++) {
+            n = (int) (Math.random() * 6);
+            m = (int) (Math.random() * 7);
+            if (i < 3) {
+                plateau.placerTrouNoir(n, m);
+                if (i > 2) {
+                    plateau.placeTrouNoir(n, m);
+                    plateau.placerDesintegrateur(n, m);
+                }
+
+            }
+        }
+
     }
-    
-    public void initialiserPartie(){
-           attribuerCouleurAuxJoueurs();
-           creeEtAffecterJeton(tab[0]);
-           creeEtAffecterJeton(tab[1]);
-           placerTrouNoirEtDesintegrateur();
+     */
+
+    public void initialiserPartie() {
+        attribuerCouleurAuxJoueurs();
+        creeEtAffecterJeton(tab[0]);
+        creeEtAffecterJeton(tab[1]);
+        //placerTrouNoirEtDesintegrateur();
     }
-    
-    public void lancerPartie(){
-        
+
+    public void lancerPartie() {
+
     }
-    
-        
+
 }
